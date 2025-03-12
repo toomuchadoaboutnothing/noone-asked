b6b2f0e3fbef9fd1033095b2a648d4e7b60f203f
toomuchadoaboutnothing <96863403+toomuchadoaboutnothing@users.noreply.github.com>
2025-03-12 20:24:24 +0800

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const optionsButton = document.getElementById('optionsButton');
-
-    // Open options page when the button is clicked
-    optionsButton.addEventListener('click', () => {
-        chrome.runtime.openOptionsPage();
-    });
-});
